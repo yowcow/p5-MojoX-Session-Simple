5fd164ebfe15cb69a26e969c08db60c498f45b83
yowcow <yowcow@gmail.com>
2014-10-07 21:43:17 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+MojoX::Session::Adapter::PSGI - It's new $module
+
+# SYNOPSIS
+
+    use MojoX::Session::Adapter::PSGI;
+
+# DESCRIPTION
+
+MojoX::Session::Adapter::PSGI is ...
+
+# LICENSE
+
+Copyright (C) yowcow.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+yowcow <yowcow@gmail.com>
---
+++ lib/MojoX/Session/Adapter/PSGI.pm
@@ -0,0 +1,39 @@
+package MojoX::Session::Adapter::PSGI;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+MojoX::Session::Adapter::PSGI - It's new $module
+
+=head1 SYNOPSIS
+
+    use MojoX::Session::Adapter::PSGI;
+
+=head1 DESCRIPTION
+
+MojoX::Session::Adapter::PSGI is ...
+
+=head1 LICENSE
+
+Copyright (C) yowcow.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+yowcow E<lt>yowcow@gmail.comE<gt>
+
+=cut
+
